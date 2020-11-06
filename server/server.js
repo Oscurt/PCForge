b36572d2bb43acc94b68d95b2b38baee4e2d5a89
Oscurt <cristian.villavicencio1@mail.udp.cl>
2020-11-06 02:51:41 -0300

    add client

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@
 const { response } = require('express');
 const express = require('express');
 const pool = require("./db");
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('server up :D')
